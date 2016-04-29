fd1a323bc6b9430785d95aa1ed7b964667fabbe8
jssmith1331 <jssmith1331@gmail.com>
2016-04-28 18:01:09 -0700

    added google maps activity

--- app/src/main/java/com/example/noellin/coupletones/MainActivity.java
+++ app/src/main/java/com/example/noellin/coupletones/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.noellin.coupletones;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,14 +19,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
@@ -49,4 +43,13 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void toMap(View view){
+
+        //Move to activity
+        Intent intent = new Intent(this, MapsActivity.class);
+
+        startActivity(intent);
+    }
+
 }
