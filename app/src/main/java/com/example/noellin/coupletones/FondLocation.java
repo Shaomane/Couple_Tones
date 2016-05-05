1bfc9778e3ac34095d5f00ddbf0ed7632b7d3310
Noel Lin <nolin9889@gmail.com>
2016-05-05 14:45:36 -0700

    Got dialog window to take in location name. Next step: Saving it.

--- app/src/main/java/com/example/noellin/coupletones/FondLocation.java
+++ app/src/main/java/com/example/noellin/coupletones/FondLocation.java
@@ -1,7 +1,28 @@
 package com.example.noellin.coupletones;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by noellin on 5/4/16.
  */
-public class FondLocation {
+public class FondLocation  {
+
+    public String locationName;
+    public LatLng latLng;
+
+    // creates a locatoin and initializes the latitude and longitude.
+    public FondLocation(double latitude, double longitude) {
+        latLng = new LatLng(latitude, longitude);
+
+    }
+    // setter for location name
+    public void setName (String locationName) {
+
+        this.locationName = locationName;
+    }
+    /* getter for location name */
+    public String getName () {
+
+        return this.locationName;
+    }
 }
--- app/src/main/java/com/example/noellin/coupletones/MapsActivity.java
+++ app/src/main/java/com/example/noellin/coupletones/MapsActivity.java
@@ -1,14 +1,22 @@
 package com.example.noellin.coupletones;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
@@ -18,6 +26,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     static ArrayList<LatLng> arrayLatLng = new ArrayList<LatLng>();
     static int locationToggle = 0;              // counter to check for addlocation toggle
+    String m_Text = "";
+
 
 
     @Override
@@ -43,7 +53,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap)
+    {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
@@ -60,25 +71,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
       //  public void addLocation() {
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener()
+        {
 
             @Override
-            public void onMapClick(LatLng point) {
-                // TODO Auto-generated method stub
+            public void onMapClick(LatLng point)
+            {
                 System.out.println(arrayLatLng);
-                //  mMap.clear();
-                if (locationToggle %2 == 0) {
-                    mMap.addMarker(new MarkerOptions().position(point));
-                    arrayLatLng.add(point);
+                if (locationToggle % 2 == 0)
+                {
+                    showDialog ();
+                    Marker marker = mMap.addMarker(new MarkerOptions()
+                                                    .position(point)
+                                                    .title(m_Text));
 
+                    arrayLatLng.add(point);
                 }
             }
+
+
         });
-        }
+    }
+    public void showDialog ()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivity.this);
+        builder.setTitle("Title");
 
-    public void addLocation (View view) {
+        // Set up the input
+        final EditText input = new EditText(MapsActivity.this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
 
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                m_Text = input.getText().toString();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+
+
+    }
+
+    public void addLocation (View view)
+    {
         locationToggle++;
+
     }
 
 
