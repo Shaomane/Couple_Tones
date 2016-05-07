9bb7d826eeef3058219e1777b98c6ef8237d7c99
Noel Lin <nolin9889@gmail.com>
2016-05-07 13:24:07 -0700

    Commit before pull

--- app/src/main/java/com/example/noellin/coupletones/SignInActivity.java
+++ app/src/main/java/com/example/noellin/coupletones/SignInActivity.java
@@ -140,8 +140,8 @@ public class SignInActivity extends AppCompatActivity implements
 
         } else {
             // Signed out, show unauthenticated UI.
-            updateUI(false);
-            loggedIn = false;
+         //   updateUI(false);
+           // loggedIn = false;
         }
     }
     // [END handleSignInResult]
